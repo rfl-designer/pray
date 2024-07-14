5d4c6c85fdeee934461ad5d475520376648c499a
Rafael Oliveira <rfldesigner@outlook.com>
2024-07-14 15:32:36 -0300

    wip

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -4,8 +4,8 @@
         <meta charset="utf-8" />
 
         <meta name="application-name" content="{{ config('app.name') }}" />
-        <meta name="csrf-token" content="{{ csrf_token() }}" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ config('app.name') }}</title>
 
