6ff8af528d2f97c6938858ab4771c811876a198f
Rafael Oliveira <rfldesigner@outlook.com>
2024-07-15 18:26:38 -0300

    fix: can access panel for email agd

--- app/Models/User.php
+++ app/Models/User.php
@@ -56,6 +56,6 @@ class User extends Authenticatable implements FilamentUser
      */
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@agdcomunicacao.com');
+        return str_ends_with($this->email, '@agdcomunicacao.com.br');
     }
 }
