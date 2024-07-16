7b184f58028eb3e91bdb706ef75fb05e3ea6ddad
Rafael Oliveira <rfldesigner@outlook.com>
2024-07-15 22:54:19 -0300

    change order from button user

--- resources/views/livewire/user-button.blade.php
+++ resources/views/livewire/user-button.blade.php
@@ -10,8 +10,8 @@ $setUserMenu = function () {
 
 ?>
 
-<div class="flex items-center justify-end p-4 w-full md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative">
-    <button type="button" wire:click="setUserMenu" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown" data-dropdown-placement="bottom">
+<div class="flex items-center justify-end p-4 w-full space-x-3 md:space-x-0 relative">
+    <button type="button" wire:click="setUserMenu" class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600">
         <span class="sr-only">Open user menu</span>
         <img class="w-8 h-8 rounded-full" src="{{ auth()->user()->avatar }}" alt="user photo">
     </button>
