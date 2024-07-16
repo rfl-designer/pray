2141296693b1e0a7834cf077b82f37f8c0109f1f
Rafael Oliveira <rfldesigner@outlook.com>
2024-07-15 21:14:35 -0300

    add new output for cron

--- cron.php
+++ cron.php
@@ -1,7 +1,7 @@
 <?php
 
 // Comando que você quer executar
-$command = "cd /home/u425289644/domains/praynow.rfloliveira.com.br && php artisan schedule:run >> /dev/null 2>&1";
+$command = "cd /home/u425289644/domains/praynow.rfloliveira.com.br && php artisan schedule:run 2> /path/to/error.log";
 
 // Executa o comando
 exec($command, $output, $return_var);
@@ -9,3 +9,7 @@ exec($command, $output, $return_var);
 // Exibe a saída do comando
 print_r($output);
 echo "Return status: " . $return_var;
+
+// Leia e exiba o conteúdo do arquivo de log de erros
+$error_log = file_get_contents('/home/u425289644/domains/praynow.rfloliveira.com.br/storage/logs/laravel.log');
+echo "Error log: " . $error_log;
