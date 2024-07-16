52498b04f3eae886b67b2708d863369589c7d1c1
Rafael Oliveira <rfldesigner@outlook.com>
2024-07-15 21:02:47 -0300

    wip

--- routes/console.php
+++ routes/console.php
@@ -3,5 +3,5 @@
 use App\Jobs\SendPrayNow;
 use Illuminate\Support\Facades\{Schedule};
 
-Schedule::job(new SendPrayNow())->everyThirtyMinutes();
-Schedule::command('queue:work --stop-when-empty')->everyThirtyMinutes();
+Schedule::job(new SendPrayNow())->everyMinute();
+Schedule::command('queue:work --stop-when-empty')->everyMinute();
